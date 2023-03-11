020012d8d4f47ec1e9c51a5a596996443c5f40da
RolandSall <roland.salloum00@outlook.com>
2023-03-11 23:48:09 +0200

    feat: initial project setup

---
+++ delivery-service/src/main/java/com/delivery/service/DeliveryService.java
@@ -0,0 +1,14 @@
+package com.delivery.service;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryService
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(DeliveryService.class, args);
+    }
+}
